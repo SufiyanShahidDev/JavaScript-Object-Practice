98027d4cd4b5b998e137944a9ec49d771e6f324e
Sufiyan Shahid <sufiyanshahid144@gmail.com>
2025-12-27 09:45:10 +0500

    Initial Commit

---
+++ app.js
@@ -0,0 +1,18 @@
+
+
+let obj = {
+    name: "Sufiyan"
+};
+
+console.log(obj.name);
+
+let myObj = {
+    myalert : function(){
+        alert("running");
+        return
+    }
+}
+
+// console.log(myObj.myalert());
+
+
