571329cfc145d08d0ac1cc5b9451c12bfb3e5ad6
Sufiyan Shahid <sufiyanshahid144@gmail.com>
2025-12-27 10:59:30 +0500

    Initial commit

--- app.js
+++ app.js
@@ -89,6 +89,136 @@
 // console.log(newObj)
 
 
+// let userData ={
+//     firstName :"Hasan",
+//     lastName :"Ashraf",
+//     userName : "hasancoder66",
+//     email:"codermhasan@gmail.com",
+//     contact:3170243866,
+//     description :"lorem impsum dolor",
+
+// }
+
+
+// let userData ={
+      
+//         "experience": [
+//             {
+//                 "_id": "68020e7532752bd6c9ae4c1c",
+//                 "user": "663681ced78314aa80690e64",
+//                 "position": "Frontend Developer",
+//                 "employmentType": "Full-Time",
+//                 "companyName": "Hiring Mine",
+//                 "city": "Karachi",
+//                 "country": "Pakistan",
+//                 "currentlyWorking": true,
+//                 "startMonth": "8",
+//                 "startYear": "2024",
+//                 "endMonth": null,
+//                 "endYear": null,
+//                 "totalDuration": 1.2449376257927447,
+//                 "description": "Built and optimized interactive React.js components with seamless API integration.\nDeveloped and maintained web applications using React.js and Redux.",
+//                 "__v": 0,
+//                 "locationType": "onsite"
+//             },
+//             {
+//                 "_id": "68020e7532752bd6c9ae4c20",
+//                 "user": "663681ced78314aa80690e64",
+//                 "position": "Frontend Developer",
+//                 "employmentType": "Part-Time",
+//                 "companyName": "Build Link",
+//                 "city": "Karachi",
+//                 "country": "Pakistan",
+//                 "currentlyWorking": false,
+//                 "startMonth": "5",
+//                 "startYear": "2024",
+//                 "endMonth": "7",
+//                 "endYear": "2024",
+//                 "totalDuration": 0.16712328767123288,
+//                 "description": "Built and optimized interactive React.js components with seamless API integration.\nDelivered responsive features for an e-commerce platform in a remote setup.",
+//                 "__v": 0,
+//                 "locationType": "remote"
+//             },
+//             {
+//                 "_id": "68020e7532752bd6c9ae4c22",
+//                 "user": "663681ced78314aa80690e64",
+//                 "position": "Frontend Developer",
+//                 "employmentType": "Part-Time",
+//                 "companyName": "Branding Hopes",
+//                 "city": "Karachi",
+//                 "country": "Pakistan",
+//                 "currentlyWorking": false,
+//                 "startMonth": "2",
+//                 "startYear": "2024",
+//                 "endMonth": "4",
+//                 "endYear": "2024",
+//                 "totalDuration": 0.16712328767123288,
+//                 "description": "Create a mobile-friendly layout using Tailwind CSS or styled-components for consistent user experience across devices.\nDevelop dynamic forms using React.js for better engagement and usability.",
+//                 "__v": 0,
+//                 "locationType": "remote"
+//             }
+//         ],
+//         "firstName": "Hasan",
+//         "isVerified": true,
+//         "isAdmin": true,
+//         "lastName": "Ashraf",
+//         "mobileVerified": true,
+//         "userName": "Hasan",
+//         "resumeUrl": "https://s3.ap-southeast-1.amazonaws.com/singabucket.hiringmine/profile/cover/1764409585596-Hasan-Frontend.pdf",
+//         "email": "codermhasan@gmail.com",
+//         "country": "Pakistan",
+//         "city": "Karachi",
+//         "dateOfBirth": null,
+//         "description": "Frontend  Developer  with  expertise  in  React.js,  JavaScript,  HTML5,  CSS3,\nTailwind CSS, and Material UI. Proven experience delivering 3+ web projects\nwith clean code and optimized performance. Skilled in API integration, state\nmanagement,  and  translating  UI  designs  into  interactive  user  experiences.\nSeeking opportunities to contribute to modern, scalable frontend solutions.",
+//         "jobTitle": "Frontend Developer",
+//         "profilePic": "https://s3.ap-southeast-1.amazonaws.com/singabucket.hiringmine/profile/image/1714851869389-for%20thumbnail.jpg",
+//         "coverPhoto": "https://s3.ap-southeast-1.amazonaws.com/singabucket.hiringmine/profile/cover/1724925197209-1.jpg",
+//         "education": [
+//             {
+//                 "_id": "68020e7732752bd6c9ae4c29",
+//                 "user": "663681ced78314aa80690e64",
+//                 "institutionName": "GOVT CITY COLLEGE",
+//                 "degree": "Intermediate",
+//                 "fieldOfStudy": "Commerce",
+//                 "startMonth": "0",
+//                 "startYear": "2022",
+//                 "endMonth": "0",
+//                 "endYear": "2024",
+//                 "grade": "C",
+//                 "currentlyProcessing": false,
+//                 "__v": 0
+//             },
+//             {
+//                 "_id": "68020e7732752bd6c9ae4c2b",
+//                 "user": "663681ced78314aa80690e64",
+//                 "institutionName": "YOUNG LEARNER’S SCHOOL",
+//                 "degree": "Matriculation",
+//                 "fieldOfStudy": "Computer Science",
+//                 "startMonth": "0",
+//                 "startYear": "2020",
+//                 "endMonth": "0",
+//                 "endYear": "2022",
+//                 "grade": "B",
+//                 "currentlyProcessing": false,
+//                 "__v": 0
+//             },
+//             {
+//                 "_id": "68020e7732752bd6c9ae4c2f",
+//                 "user": "663681ced78314aa80690e64",
+//                 "institutionName": "SMIT (Saylani Mass IT Training)",
+//                 "degree": "Diploma",
+//                 "fieldOfStudy": "Web Development",
+//                 "startMonth": "0",
+//                 "startYear": "2023",
+//                 "endMonth": "11",
+//                 "endYear": "2023",
+//                 "grade": "A",
+//                 "currentlyProcessing": false,
+//                 "__v": 0
+//             }
+//         ],
+
+
 let newObject = {
     username : "Sufiyan",
     fatherName : "Shahid",
