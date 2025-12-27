82fb6d1f3acd903dbc161a281a44f1a787a0a6b8
Sufiyan Shahid <sufiyanshahid144@gmail.com>
2025-12-27 10:54:43 +0500

    Initial commit

--- app.js
+++ app.js
@@ -1,18 +1,27 @@
 
 
-let obj = {
-    name: "Sufiyan"
-};
+// let obj = {
+//     name: "Sufiyan"
+// };
 
-console.log(obj.name);
+// console.log(obj.name);
 
-let myObj = {
-    myalert : function(){
-        alert("running");
-        return
-    }
-}
+// let myObj = {
+//     myalert : function(){
+//         alert("running");
+//         return
+//     }
+// }
 
 // console.log(myObj.myalert());
 
+let newObject = {
+    username : "Sufiyan",
+    fatherName : "Shahid",
+    course : "I am learning web development from SMIT and I have learned many things from this course.",
+    CNIC : 422013434545
+}
 
+let studentName = document.getElementById("title").innerText = "Fullname: " + newObject.username + " " + newObject.fatherName;
+
+let myPara = document.getElementById("para").innerText = newObject.course
